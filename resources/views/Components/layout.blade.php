33ff8679bc676ec85cabe1af2ca0a93e27dd36f7
kayii <135094209+denisesarona@users.noreply.github.com>
2025-03-18 08:32:47 +0800

    Created navbar according to mockup

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -9,13 +9,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <style>
+        .navbar-light .navbar-nav .nav-link {
+            color: #000;
+        }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img id="MDB-logo" src="https://mdbcdn.b-cdn.net/wp-content/uploads/2018/06/logo-mdb-jquery-small.png" alt="MDB Logo"
-                draggable="false" height="30" />
+                <img id="MDB-logo" src="{{ asset('images/ASHILOGO.png') }}" alt="MDB Logo" draggable="false" height=60">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,17 +28,17 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto align-items-center">
                     <li class="nav-item">
-                        <a class="nav-link mx-2" href="#"><i class="fas fa-plus-circle pe-2"></i>Post</a>
+                        <a class="nav-link mx-2" href="#">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mx-2" href="#"><i class="fas fa-bell pe-2"></i>Alerts</a>
+                        <a class="nav-link mx-2" href="#">ABOUT</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mx-2" href="#"><i class="fas fa-heart pe-2"></i>Trips</a>
+                        <a class="nav-link mx-2" href="#">TRACK ENROLLMENT</a>
                     </li>
-                    <li class="nav-item ms-3">
-                        <a class="btn btn-dark btn-rounded" href="#">Sign in</a>
-                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-2" href="#">CONTACT US</a>
+                    </li>   
                 </ul>
             </div>
         </div>
