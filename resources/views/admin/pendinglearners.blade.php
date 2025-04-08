02a1e39aa2befcf1f1f753c9a2c25b484b41d363
kayii <135094209+denisesarona@users.noreply.github.com>
2025-04-08 14:40:54 +0800

    Added list layout in pending learners page

--- resources/views/admin/pendinglearners.blade.php
+++ resources/views/admin/pendinglearners.blade.php
@@ -1,3 +1,22 @@
 <x-admin-dashboard-layout>
-    Pending Learners
+    <div class="min-h-screen flex flex-col items-center bg-white p-4 shadow-lg rounded-xl">
+        <div class="pt-8">
+            <h1 class="text-4xl font-bold text-center">PENDING LEARNERS</h1>
+        </div>
+        <div class="w-full max-w-3xl mt-8">
+            <table class="w-full text-center">
+                <thead>
+                    <tr class="text-gray-600 text-lg font-semibold">
+                        <th class="px-8 py-2 hidden md:table-cell">ID</th>
+                        <th class="px-8 py-2">FULL NAME</th>
+                        <th class="px-8 py-2 hidden md:table-cell">GRADE LEVEL</th>
+                        <th class="px-8 py-2 hidden md:table-cell">STATUS</th>
+                        <th class="px-8 py-2">VIEW DETAILS</th>
+                        <th class="px-8 py-2 hidden md:table-cell">REMOVE</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 </x-admin-dashboard-layout>
+
