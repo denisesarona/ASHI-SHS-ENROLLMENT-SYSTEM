59a4e232c5d77c0b135ea7f297275ebfb5302955
kayii <135094209+denisesarona@users.noreply.github.com>
2025-04-10 16:01:50 +0800

    Created learner details page

--- resources/views/admin/pendinglearners.blade.php
+++ resources/views/admin/pendinglearners.blade.php
@@ -30,7 +30,7 @@
                         </td>
                         <td class="px-4 py-3 hidden sm:table-cell">
                             <button data-modal-target="popup-modal-{{ $learner->id }}" data-modal-toggle="popup-modal-{{ $learner->id }}" class=" text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" type="button">
-                                REMOVE ADMIN
+                                REMOVE
                             </button>
                             <div id="popup-modal-{{ $learner->id }}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                 <div class="relative p-4 w-full max-w-md max-h-full">
