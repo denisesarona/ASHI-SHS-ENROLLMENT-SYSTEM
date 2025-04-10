2bef418aecd8182ba547734e2790c86bc206e0f7
kayii <135094209+denisesarona@users.noreply.github.com>
2025-04-10 16:12:13 +0800

    Update input in learner details

--- resources/views/admin/learnerdetails.blade.php
+++ resources/views/admin/learnerdetails.blade.php
@@ -9,9 +9,14 @@
                 @method('PUT')
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div>
-                        <label class="block font-semibold text-lg text-gray-700 mb-2">Name</label>
+                        <label class="block font-semibold text-lg text-gray-700 mb-2">Last Name</label>
                         <input type="text" class="w-full p-3 border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500" 
                                name="name" value="{{ $learner->last_name }}">
+                    </div>
+                    <div>
+                        <label class="block font-semibold text-lg text-gray-700 mb-2">First Name</label>
+                        <input type="text" class="w-full p-3 border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                               name="name" value="{{ $learner->first_name }}">
                     </div>
                     <div>
                         <label class="block font-semibold text-lg text-gray-700 mb-2">Email</label>
