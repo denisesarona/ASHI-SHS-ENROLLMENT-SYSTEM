01a54aa4cbfc4ae2ebe1bdd3e4505b96d3a2eb9a
kayii <135094209+denisesarona@users.noreply.github.com>
2025-04-24 08:50:38 +0800

    Added fillable for the model

--- app/Models/Summary.php
+++ app/Models/Summary.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Summary extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'school_year',
+        'grade_level',
+        'last_name',
+        'first_name',
+        'middle_name',
+        'extension_name',
+        'lrn',
+        'birthdate',
+        'age',
+        'gender',
+        'beneficiary',
+        'street',
+        'baranggay',
+        'municipality',
+        'province',
+        'guardian_name',
+        'guardian_contact',
+        'relationship_guardian',
+        'last_sy',
+        'last_school',
+        'learner_category',
+        'grade10_section',
+        'image',
+        'chosen_track',
+        'chosen_strand',
+        'section',
+    ];
 }
