2e935f658977a14573eb485b1159ac1f58c76591
kayii <135094209+denisesarona@users.noreply.github.com>
2025-04-26 14:13:25 +0800

    Added fillable to section model

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Section extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'strand'];
+
+    public function learners()
+    {
+        return $this->hasMany(Learner::class);
+    }
 }
