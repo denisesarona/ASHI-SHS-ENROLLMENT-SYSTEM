48ae86d7d99245f72be2dc4028bc5b761f8bb605
kayii <135094209+denisesarona@users.noreply.github.com>
2025-05-04 21:52:41 +0800

    Fix layout

--- resources/views/components/admin-nav-link.blade.php
+++ resources/views/components/admin-nav-link.blade.php
@@ -4,7 +4,6 @@
     'class' => 'block w-full rounded-md px-3 py-2 text-md font-large ' . 
               ($active ? 'text-white bg-blue-500' : 'dark:text-white p-2 dark:hover:bg-blue-500 hover:text-white')
 ]) }}
-    aria-current="{{ $active ? 'page' : 'false' }}"
->
+    aria-current="{{ $active ? 'page' : 'false' }}">
     {{ $slot }}
 </a>
