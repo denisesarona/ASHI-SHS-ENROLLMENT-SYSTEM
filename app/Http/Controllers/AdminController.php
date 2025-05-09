7cf7b3fc27b4a509597c9a911af37b3818acc621
kayii <135094209+denisesarona@users.noreply.github.com>
2025-05-09 09:12:52 +0800

    Created route and query for adding new learner in admin side

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -688,5 +688,90 @@ class AdminController extends Controller
             'selectedSection' => $sectionName,
         ]);   
     }
+
+
+    public function addNewLearner(Request $request){
+         
+        $validator = Validator::make($request->all(), [
+            'school_year' => 'required|string|max:255',
+            'grade_level' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'middle_name' => 'string|max:255',
+            'extension_name' => 'nullable|string',
+            'lrn' => 'required|string|max:255',
+            'birthdate' => 'required|date',
+            'age' => 'required|integer',
+            'gender' => 'string|max:255',
+            'beneficiary' => 'string|max:255',
+            'street' => 'required|string|max:255',
+            'baranggay' => 'required|string|max:255',
+            'municipality' => 'required|string|max:255',
+            'province' => 'required|string|max:255',
+            'guardian_name' => 'required|string|max:255',
+            'guardian_contact' => 'required|string|max:255',
+            'relationship_guardian' => 'nullable|string',
+            'last_sy' => 'required|string|max:255',
+            'last_school' => 'required|string|max:255',
+            'learner_category' => 'required|string|max:255',
+            'grade10_section' => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,jpg,png,gif,svg|max:5120',
+            'chosen_strand' => 'required|string',
+            'status' => 'required|string|max:255',
+        ]);
+
+        $imagePath = null;
+        if($request->hasFile('image')){
+            $imagePath = $request->file('image')->store('image', 'public');
+        }
+
+        if ($validator->fails()) {
+            dd($validator->errors()); // Show the exact error messages
+            return redirect()->back()->withErrors($validator)->withInput();
+        }        
+
+        try {
+
+            $learner = Learner::where('lrn', $request->lrn)
+                          ->where('last_name', $request->last_name)
+                          ->first();
+    
+            if (!$learner) {   
+                $learner = Learner::create([
+                    'school_year' => $request->school_year,
+                    'grade_level' => $request->grade_level,
+                    'last_name' => $request->last_name,
+                    'first_name' => $request->first_name,
+                    'middle_name' => $request->middle_name,
+                    'extension_name' => $request->extension_name,
+                    'lrn' => $request->lrn,
+                    'birthdate' => $request->birthdate,
+                    'age' => $request->age,
+                    'gender' => $request->gender,
+                    'beneficiary' => $request->beneficiary,
+                    'street' => $request->street,
+                    'baranggay' => $request->baranggay,
+                    'municipality' => $request->municipality,
+                    'province' => $request->province,
+                    'guardian_name' => $request->guardian_name,
+                    'guardian_contact' => $request->guardian_contact,
+                    'relationship_guardian' => $request->relationship_guardian,
+                    'last_sy' => $request->last_sy,
+                    'last_school' => $request->last_school,
+                    'learner_category' => $request->learner_category,
+                    'grade10_section' => $request->grade10_section,
+                    'image' => $imagePath,
+                    'chosen_strand' => $request->chosen_strand,
+                    'status' => $request->status,
+                ]);
+                return redirect()->route('controlnum', ['id' => $learner->id])->with('success', 'Learner enrollment successful.');
+            } else {
+                return redirect()->route('enrollment')->with('error', 'Learner already submitted an enrollment form.');
+            }
+            
+        } catch (\Exception $e) {   
+            return redirect()->back()->withErrors(['error' => $e->getMessage()])->withInput();
+        }
+    }
 }    
     
